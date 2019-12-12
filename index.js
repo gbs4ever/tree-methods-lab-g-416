a2e8946bc665a50e86417e2ddf3064058d199d93
Simcha Greenbaum <gbs4ever2@gmail.com>
2019-12-12 22:35:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
-class BinarySearchTree {
-  constructor(val){
-    this.val = val;
-    this.left = null;
-    this.right = null;
+function findOrAdd(currentNode, newNode) {
+  if (currentNode.data === newNode.data) {
+    return true
   }
-  preorder(array = []){
-    array.push(this.val)
- console.log(array)
-  if(this.left){
-    this.left.preorder(array)
+  if (newNode.data > currentNode.data) {
+      //no right node exists
+    if (!currentNode.right) {
+        //create node && append to right subtree
+      return currentNode.right = newNode
+      } else {
+     return  findOrAdd(currentNode.right, newNode)
+      }
+    } else {
+      //left subtree
+    if (!currentNode.left) {
+        //no left node exists
+        //create node && append to left subtree
+      return currentNode.left = newNode
+      } else {
+    return  findOrAdd(currentNode.left, newNode)
+      }
+    }
   }
+  
 
-  if(this.right){
-    this.right.preorder(array)
+function inOrder(currentNode) {
+  if (currentNode.left) {
+   inOrder(currentNode.left)
+      }
+  console.log(currentNode.data)
+  if (currentNode.right) {
+      inOrder(currentNode.right)
+      }
+
+    }
+function max(currentNode) {
+  if (currentNode.right) {
+    return max(currentNode.right)
+  } else {
+    return currentNode;
   }
-  console.log(array)
-return array
+}
+
+function min(currentNode) {
+  if (currentNode.left) {
+    return min(currentNode.left)
+  } else {
+    return currentNode;
+  }
+}
 }
